549e778e3e39fb12cdf775b519f837394304af80
andam adam <79620025+andam20@users.noreply.github.com>
2023-04-08 16:55:56 +0300

    return them with their salary

--- app/Http/Controllers/APIEmployeeController.php
+++ app/Http/Controllers/APIEmployeeController.php
@@ -55,7 +55,38 @@ class APIEmployeeController extends Controller
 
         return response()->json($data);
     }
-    
+
+    public function getEmployeeManagersSalary($id)
+    {
+        $employee = Employee::findOrFail($id);
+
+        $managerLine = $employee->managerLine;
+        $manager = $managerLine->manager;
+        $founder = $manager->founder;
+        $employees = $managerLine->employees;
+
+        $data = [
+            'managerLine' => [
+                'id' => $managerLine->id,
+                'name' => $managerLine->name,
+                'salary' => $managerLine->salary,
+            ],
+            'manager' => [
+                'id' => $manager->id,
+                'name' => $manager->name,
+                'salary' => $manager->salary,
+            ],
+            'founder' => [
+                'id' => $founder->id,
+                'name' => $founder->name,
+                'salary' => $founder->salary,
+
+            ],
+            'employees' => $employees->pluck('name')
+        ];
+
+        return response()->json($data);
+    }
 
 
     /**
@@ -107,7 +138,7 @@ class APIEmployeeController extends Controller
             "Expires" => "0"
         );
 
-        $columns = array('name', 'age', 'salary', 'gender','hired date', 'job title', 'managers');
+        $columns = array('name', 'age', 'salary', 'gender', 'hired date', 'job title', 'managers');
 
         $callback = function () use ($employees, $columns) {
             $file = fopen('php://output', 'w');
